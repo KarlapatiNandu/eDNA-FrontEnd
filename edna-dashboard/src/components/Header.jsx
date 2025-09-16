a2d718e5f216b299dfa560a5dab71b1d10900429
KN-1-dev <nandu211020@gmail.com>
2025-09-16 20:17:10 +0530

    Created the Header component

--- edna-dashboard/src/components/Header.jsx
+++ edna-dashboard/src/components/Header.jsx
@@ -6,3 +6,39 @@ import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
+const Header = () => {
+    return (
+        // This is main box/div for enclosing the header section
+        <Box
+            sx = {{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between', // The two boxes should be at extremes
+                p:2, // padding of 16px
+                borderBottom: '1px solid #e0e0e0',
+            }}
+        >
+            {/* Top left -> Logo and Title */}
+            <Box sx={{ display: 'flex', alignItems: 'center'}}>
+                <img src='#' alt = "eDNA Logo" style={{height:32, marginRight: 12}} />
+                <Typography variant="h6" component="h1" sx ={{fontWeight: 'bold'}}>
+                    eDNA Dashboard
+                </Typography>
+            </Box>
+            {/*Top Right -> Search and User Icons*/}
+            <Box sx={{display:'flex', alignItems:'center'}}>
+                <IconButton sx={{ mx : 1}}>
+                    <SearchIcon/>
+                </IconButton>
+                <IconButton sx={{ mx:1}}>
+                    <NotificationsIcon/>
+                </IconButton>
+                <IconButton sx = {{mx:1}}>
+                    <PersonIcon/>
+                </IconButton>
+            </Box>
+        </Box>
+    );
+};
+
+export default Header;
