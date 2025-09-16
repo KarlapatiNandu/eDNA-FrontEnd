af47078322587ab4024b5533fbb6f8a0be9eed90
KN-1-dev <nandu211020@gmail.com>
2025-09-16 19:51:14 +0530

    Initial commit

---
+++ edna-dashboard/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
